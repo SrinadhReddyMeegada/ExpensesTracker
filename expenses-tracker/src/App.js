eee24743a3f10ad1caf4e5b123da94a6816c2b9f
SrinadhReddyMeegada <srinadh.meegada@gmail.com>
2023-10-03 23:19:06 +0530

    added ExpensesItem

--- expenses-tracker/src/App.js
+++ expenses-tracker/src/App.js
@@ -1,7 +1,10 @@
+import ExpensesItem from "./Components/ExpensesItem/ExpensesItem";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <ExpensesItem />
     </div>
   );
 }
---
+++ expenses-tracker/src/Components/ExpensesItem/ExpensesItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ExpensesItem.css";
+const ExpensesItem = () => {
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+        <div className="expense-item__price">Amount</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesItem;
