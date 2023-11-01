28af07791cf4a442a346478e1877f8db40caad89
srinadhreddy <srinadhreddy@yupptv.com>
2023-11-01 22:57:00 +0530

    Initial commit for blog

---
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -0,0 +1,51 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentImage from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
+function App() {
+  const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+  function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+  function Header() {
+    const description = reactDescriptions[genRandomInt(2)];
+    return (
+      <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    );
+  }
+
+  function CoreConcepts({ title, image, description }) {
+    return (
+      <li>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+  }
+  return (
+    <div>
+      <Header></Header>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
